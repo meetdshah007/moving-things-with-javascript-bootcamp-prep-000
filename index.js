a389346ba3076b311cdeb989ea16af986ec268cc
Meet Shah <meetdshah007@gmail.com>
2019-02-27 12:49:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 
 function moveDodgerRight() {
   document.addEventListener('keydown', function(e) {
-    if (e.which === 37) {
+    if (e.which === 39) {
       moveDodgerLeft()
     }
   })
